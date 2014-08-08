299c1dbfa0542e4197a4cbf6821c1093937d0d68
Ben Goldberger <bwgoldberger@gmail.com>
2014-08-08 09:53:03 -0500

    Finally fixed config

--- application/config/app_general_configs.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    $config['num_photos_select'] = 3;
-
-    //facebook api
-    $config['tinyPhotoNum'] = 1;
-    $config['smallPhotoNum'] = 3;
-    $config['bigPhotoNum'] = 6;
-?>
--- application/config/javascript.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['javascript_location'] = "/assets/js/jquery.js";
-//$config['javascript_ajax_img'] = 'images/ajax-loader.gif';
-
-?>
