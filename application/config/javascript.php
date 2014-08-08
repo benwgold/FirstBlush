ec8d7e78d377080924eb9f7eaf15f0eb65bb0a9a
Ben Goldberger <bwgoldberger@gmail.com>
2014-08-08 09:42:27 -0500

    Final config fix

---
+++ application/config/javascript.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['javascript_location'] = "/assets/js/jquery.js";
+//$config['javascript_ajax_img'] = 'images/ajax-loader.gif';
+
+?>
